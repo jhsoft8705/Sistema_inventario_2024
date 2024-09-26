72d7e06a317d3cf530e67e01a96a5d160547d065
jhsoft8705 <jhonalexvillaflores@gmail.com>
2024-09-26 18:16:53 -0500

    conexion prod

--- config/conexion.php
+++ config/conexion.php
@@ -4,10 +4,14 @@
     /* TODO: Inicio de Session */
     class Conectar{
         protected $dbh;
+
         protected function Conexion(){
+            $dbname = "u270537271_db_inventory";  
+            $dbuser = "u270537271_inventariouser";  
+            $dbpass = "9c3[h5f^U";  
             try{
                 /* TODO: Cadena de Conexion */
-                $conectar = $this->dbh=new PDO("mysql:host=localhost;dbname=u270537271_db_inventory","u270537271_inventoryuser","4b@Oj[G=QUc");
+                $conectar = $this->dbh=new PDO("mysql:host=localhost;dbname=$dbname", $dbuser, $dbpass);  
                 return $conectar;
             }catch (Exception $e){
                 /* TODO: En caso de error mostrar mensaje */
@@ -18,7 +22,7 @@
         } 
         public static function ruta(){
 
-            return "http://jhonalexvillaflores.website/"; 
+            return "http://localhost/Sistema_inventario_2024/"; 
         }
     }
 ?>
